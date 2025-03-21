e00450de365296538a6a1ea813c469991525bfbe
TruptiVadaliya <truptivadaliya46@gmail.com>
2025-03-21 16:43:59 +0530

    c

--- Crud/crud.js
+++ Crud/crud.js
@@ -1,105 +1,52 @@
-const form = document.getElementById("myForm"),
+var form = document.getElementById("myForm"),
     imgInput = document.querySelector(".img"),
     file = document.querySelector("#imgInput"),
-    name = document.querySelector("#name"),
-    age = document.querySelector("#age"),
-    city = document.querySelector("#city"),
-    email = document.querySelector("#email"),
-    phone = document.querySelector("#phone"),
-    post = document.querySelector("#post"),
-    sDate = document.querySelector("#sDate"),
+    name = document.querySelector("name"),
+    age = document.querySelector("age"),
+    city = document.querySelector("city"),
+    email = document.querySelector("email"),
+    phone = document.querySelector("phone"),
+    post = document.querySelector("post"),
+    sData = document.querySelector("sData"),
     submitBtn = document.querySelector(".submit"),
-    userInput = document.querySelector("#data");
+    userInput = document.querySelector("data")
 
-let getData = localStorage.getItem('userProfile') ? JSON.parse(localStorage.getItem('userProfile')) : [];
 
+let getData = localStorage.getItem('userProfile') ? JSON.parse(localStorage.getItem('userProfile')) : []
+
+let isData = false, editId
 file?.addEventListener("change", function () {
+    console.log("File selected:", this.files[0]); // Debugging output
+});
+file.onchange = function () {
+    console.log("hgfhgfghf", file.files[0])
     if (file.files[0].size < 1000000) {
         var fileReader = new FileReader();
+
         fileReader.onload = function (e) {
-            imgInput.src = e.target.result;
-        };
-        fileReader.readAsDataURL(file.files[0]);
+            imgUrl = e.target.result
+            imgInput.src = imgUrl
+        }
+
+        fileReader.readAsDataURL(file.files[0])
     } else {
-        alert("This file is too large!");
+        alert("this file is too large!");
     }
-});
+}
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
     const information = {
-        picture: imgInput.src ? imgInput.src : "img/user-icon.webp",
+        picture: imgInput.src == undefined ? "img/user-icon.webp" : imgInput.src,
         employeeName: name.value,
         employeeAge: age.value,
         employeeCity: city.value,
         employeeEmail: email.value,
         employeePhone: phone.value,
         employeePost: post.value,
-        startDate: sDate.value
-    };
+        startDate: sData.value
+    }
 
-    getData.push(information);
-    localStorage.setItem('userProfile', JSON.stringify(getData));
-    displayData();
-    form.reset();
-});
-
-function displayData() {
-    userInput.innerHTML = "";
-    getData.forEach((user, index) => {
-        userInput.innerHTML += `
-            <tr>
-                <td>${index + 1}</td>
-                <td><img src="${user.picture}" alt="" style="width: 60px;height: 40px;"></td>
-                <td>${user.employeeName}</td>
-                <td>${user.employeeAge}</td>
-                <td>${user.employeeCity}</td>
-                <td>${user.employeeEmail}</td>
-                <td>${user.employeePhone}</td>
-                <td>${user.employeePost}</td>
-                <td>${user.startDate}</td>
-                <td>
-                    <button class="btn btn-primary" onclick="editUser(${index})"><i class="bi bi-pencil-square"></i></button>
-                    <button class="btn btn-danger" onclick="deleteUser(${index})"><i class="bi bi-trash"></i></button>
-                </td>
-            </tr>
-        `;
-    });
-}
-
-displayData();
-
-function deleteUser(index) {
-    getData.splice(index, 1);
-    localStorage.setItem('userProfile', JSON.stringify(getData));
-    displayData();
-}
-
-function editUser(index) {
-    let user = getData[index];
-    name.value = user.employeeName;
-    age.value = user.employeeAge;
-    city.value = user.employeeCity;
-    email.value = user.employeeEmail;
-    phone.value = user.employeePhone;
-    post.value = user.employeePost;
-    sDate.value = user.startDate;
-    imgInput.src = user.picture;
-
-    submitBtn.onclick = function () {
-        user.employeeName = name.value;
-        user.employeeAge = age.value;
-        user.employeeCity = city.value;
-        user.employeeEmail = email.value;
-        user.employeePhone = phone.value;
-        user.employeePost = post.value;
-        user.startDate = sDate.value;
-        user.picture = imgInput.src;
-        
-        getData[index] = user;
-        localStorage.setItem('userProfile', JSON.stringify(getData));
-        displayData();
-        form.reset();
-    };
-}
+    // if(!isEdit)
+})
