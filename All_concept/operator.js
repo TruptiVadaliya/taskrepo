6c60e15594da5d45bdd1eba8c5ac99b92b863451
TruptiVadaliya <truptivadaliya46@gmail.com>
2025-05-13 12:44:14 +0530

    Search

---
+++ All_concept/operator.js
@@ -0,0 +1,56 @@
+/*************** Arithmetic Operators ******************* */
+
+// let x = 20;
+// let y = 10;
+
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+
+
+
+// let a = 10;
+
+// a += 5;
+// a -= 5;
+// a *= 5;
+// a /= 5;
+// a %= 5;
+// console.log(a);
+
+
+
+// let a = 20;
+// let b = '20';
+
+
+// console.log(a == b);
+// console.log(a === b);
+// console.log(a != b);
+// console.log(a !== b);
+// console.log(a > 3);
+// console.log(a < 5);
+// console.log(a >= 5);
+// console.log(a <= 6);
+
+
+// let x = 5;
+// let y = 15;
+// let z = 20;
+
+// console.log(x < y && y < z); 
+// console.log(x > y || y < z); 
+// console.log(!(x > y)); 
+
+
+// let a = 10; 
+// let b = 15;
+// console.log(a & b);
+// console.log(a | b);
+// console.log(a ^ b); 
+// console.log(~a);
+// console.log(a << 1);
+// console.log(a >> 1);
